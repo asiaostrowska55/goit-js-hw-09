c1bbd0f7e1e71719d4da030f6a0e03f8c4ee08b5
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-15 20:53:29 +0100

    zadanie 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const jsStart = document.querySelector("[data-start]");
+const jsStop = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+let timerID = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+jsStart.addEventListener("click", () => {
+  jsStart.disabled = true;
+  timerID = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+jsStop.addEventListener("click", () => {
+  jsStop.disabled = true;
+  jsStart.disabled = false;
+  clearInterval(timerID);
+});
