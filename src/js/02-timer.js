b65ea92feac22a5ae12f736de348484deafe7006
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-16 22:29:26 +0100

    zad 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-// import Notiflix from "notiflix";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const jsStart = document.querySelector("[data-start]");
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+// const isSuccess = false;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// // Will run first
+// console.log("Before promise.then()");
+
+// // Registering promise callbacks
+// promise.then(
+//   // onResolve will run third or not at all
+//   (value) => {
+//     console.log("onResolve call inside promise.then()");
+//     console.log(value); // "Success! Value passed to resolve function"
+//   },
+//   // onReject will run third or not at all
+//   (error) => {
+//     console.log("onReject call inside promise.then()");
+//     console.log(error); // "Error! Error passed to reject function"
+//   }
+// );
+
+// // Will run second
+// console.log("After promise.then()");
